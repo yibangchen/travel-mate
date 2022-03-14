8b1b2cd13fe06e3d0ec2e1ea4000cd94c0a13196
Yibang Chen <ychen622@gmail.com>
2022-03-14 23:45:06 +0800

    basic trips controller and service

--- src/trips/trips.controller.ts
+++ src/trips/trips.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Body, Query, Delete, Get, Post } from '@nestjs/common';
+import { TripsService } from './trips.service';
 
 @Controller('trips')
-export class TripsController {}
+export class TripsController {
+  constructor(private tripsService: TripsService) { }
+
+  @Get()
+  async getTrips() {
+    const trips = await this.tripsService.getTrips();
+    return trips;
+  }
+
+  @Get(':tripID')
+  async getTrip(@Param('tripID') tripID) {
+    const trip = await this.tripsService.getTrip(tripID);
+    return trip;
+  }
+
+  @Post()
+  async addTrip() {
+    // to be added
+  }
+
+  @Delete()
+  async deleteTrip(@Query() query) {
+    const trips = await this.tripsService.deleteTrip(query.tripID);
+    return trips;
+  }
+}
--- src/trips/trips.service.ts
+++ src/trips/trips.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
 
 @Injectable()
-export class TripsService {}
+export class TripsService {
+  trips = [];
+
+  getTrips(): Promise<any> {
+    return new Promise(resolve => resolve(this.trips));
+  }
+
+  getTrip(tripID): Promise<any> {
+    const id = Number(tripID);
+    return new Promise(resolve => {
+      const trip = this.trips.find(trip => trip.id === id);
+      if (!trip) throw new HttpException(`Trip ${tripID} does not exist!`, 404);
+      resolve(trip);
+    });
+  }
+
+  addTrip(trip): Promise<any> {
+    return new Promise(resolve => {
+      this.trips.push(trip);
+      resolve(this.trips);
+    })
+  }
+
+  deleteTrip(tripID): Promise<any> {
+    const id = Number(tripID);
+    return new Promise(resolve => {
+      let index = this.trips.findIndex(trip => trip.id === id);
+      if (index === -1) throw new HttpException(`Trip ${tripID} does not exist!`, 404);
+      this.trips.splice(1, index);
+      resolve(this.trips);
+    });
+  }
+}
